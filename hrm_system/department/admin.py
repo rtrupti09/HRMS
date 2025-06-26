54b1f3dd8d61a3021ffaf5e8375e56053c268024
trupti <rtrupti78@gmail.com>
2025-06-26 22:07:34 +0530

    admin.py

--- hrm_system/department/admin.py
+++ hrm_system/department/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Department, Role
+from department.models import Department, Role
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
